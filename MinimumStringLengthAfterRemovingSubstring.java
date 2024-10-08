3f0897377971f28c90fad74f4f815fafc2b1f6c8
Bhanu Pratap <136685078+Bhanupratap83@users.noreply.github.com>
2024-10-08 18:35:55 +0530

    2696
    . Minimum String Length After Removing Substrings

---
+++ MinimumStringLengthAfterRemovingSubstring.java
@@ -0,0 +1,18 @@
+public class MinimumStringLengthAfterRemovingSubstring{
+    class Solution {
+        public int minLength(String s) {
+            Stack<Character> st = new Stack<>();
+    
+            for(char c : s.toCharArray()){
+                if(!st.isEmpty() && c == 'B' && st.peek() == 'A'){
+                    st.pop();
+                } else if(!st.isEmpty() && c == 'D' && st.peek() == 'C'){
+                    st.pop();
+                } else {
+                    st.push(c);
+                }
+            }
+            return st.size();
+        }
+    }
+}

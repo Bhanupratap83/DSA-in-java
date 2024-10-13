107d2f55f955f4ee67970247828e49038db63fd9
Bhanu Pratap <136685078+Bhanupratap83@users.noreply.github.com>
2024-10-13 04:38:32 -0700

    771. Jewels And Stone

---
+++ JewelsAndStone.java
@@ -0,0 +1,12 @@
+public class JewelsAndStone{
+	public int JewelsStone(String jewels, String stones){
+		int count = 0;
+		for(int i=0; i<jewels.length(); i++){
+			for(int j=0; j<stones.length(); j++){
+				if(jewels.charAt(i) == stones.charAt(j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
